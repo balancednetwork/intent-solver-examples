a15c32a75a7f0bf973108c1dda606f1d17e57b23
Robi <robi.markac@gmail.com>
2024-11-25 12:57:25 +0100

    feat: react example (#1)

---
+++ apps/react-example/src/components/ConnectEvmWalletButton.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {EvmProvider} from "balanced-solver-sdk";
+import {useConnect, useSwitchChain} from "wagmi";
+import { injected } from 'wagmi/connectors'
+import {useMultiWallet} from "@/contexts/MultiWalletContext";
+import {arbitrum} from "wagmi/chains";
+import {Button} from "@/components/ui/button";
+
+
+export default function ConnectEvmWalletButton() {
+  const { setEvmProvider } = useMultiWallet()
+  const { connectAsync } = useConnect();
+  const { switchChain } = useSwitchChain();
+
+
+  const onConnectClick = async () => {
+    const { accounts } = await connectAsync({ connector: injected() })
+    console.log("Connected accounts", accounts)
+    switchChain({
+      chainId: arbitrum.id,
+    })
+    setEvmProvider(new EvmProvider({
+      userAddress: accounts[0],
+      chain: "arb",
+      provider: (window as any).hanaWallet.ethereum,
+    }));
+  }
+
+  return (
+    <Button onClick={() => onConnectClick()}>
+      Connect Evm Wallet
+    </Button>
+  )
+}
---
+++ apps/react-example/src/components/ConnectSuiWalletButton.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {Button} from "@/components/ui/button";
+import {useMultiWallet} from "@/contexts/MultiWalletContext";
+import {useConnectWallet, useWallets} from "@mysten/dapp-kit";
+import { SuiClient, getFullnodeUrl } from "@mysten/sui/client";
+import {SuiProvider} from "balanced-solver-sdk";
+
+export default function ConnectSuiWalletButton() {
+  const { setSuiProvider } = useMultiWallet()
+  const suiWallets = useWallets();
+  const { mutateAsync: suiConnectAsync } = useConnectWallet();
+
+  const onConnectClick = async () => {
+    const wallet = suiWallets[0];
+
+    if (wallet) {
+      const { accounts } = await suiConnectAsync({ wallet: wallet })
+      console.log("Connected accounts", accounts)
+      const account = accounts[0];
+
+      if (account) {
+        setSuiProvider(new SuiProvider({
+          wallet,
+          account,
+          client: new SuiClient({ url: getFullnodeUrl("mainnet") })
+        }))
+      } else {
+        alert("No SUI account selected!")
+      }
+    } else {
+      alert("No SUI wallet connected!")
+    }
+  }
+
+  return (
+        <Button onClick={() => onConnectClick()}>
+          Connect Sui Wallet
+        </Button>
+  )
+}
