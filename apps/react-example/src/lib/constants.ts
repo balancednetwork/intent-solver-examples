029f57ad80c182d558de6155a91b90958d8b2b42
MarkacRobi <robi.markac@gmail.com>
2025-04-07 11:43:35 +0200

    chore: update solver url to main

--- apps/react-example/src/lib/constants.ts
+++ apps/react-example/src/lib/constants.ts
@@ -1,5 +1,5 @@
 import {IntentService} from "icon-intents-sdk";
 
 export const intentService = new IntentService({
-  solverApiEndpoint: "https://staging-solver.iconblockchain.xyz"
+  solverApiEndpoint: "https://solver.iconblockchain.xyz"
 })
